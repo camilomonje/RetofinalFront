bf4b3645484c366244f8a7432388dd6efd9d9f11
Mauricio Torres <otarksk@gmail.com>
2022-04-23 12:17:05 -0300

    Form navigation DateField y ContainerForm agregados

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Home from './components/Pages/Home'
 
 import { ThemeProvider} from '@mui/material/styles';
 import { theme } from './components/theme/theme';
+import { ContainerForm } from './components/Forms/ContainerForm';
 
 
 function App() {
   return (
     <ThemeProvider theme={theme} >
       <Home />
+      <ContainerForm />
     </ThemeProvider>
     
   );
---
+++ src/components/Forms/FormNavigation.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Button } from '@mui/material'
+
+
+export const FormNavigation = (props) => {
+  return (
+    <div
+        style={{
+            display: 'flex',
+            marginTop: 50,
+            justifyContent: 'space-between'
+        }}
+    >
+        {props.hasPrevious && <Button onClick={props.onBackClick} type='button' variant='contained' >Back</Button>}
+
+        <Button variant='outlined' type='submit' color='primary' >{props.isLastStep ? 'Submit' : 'Next'}</Button>
+    </div>
+  )
+}
