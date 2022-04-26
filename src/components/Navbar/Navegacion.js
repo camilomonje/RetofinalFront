57d0ed1ad40611860e1ec7477a29ef1b5a55a878
veronicapava <veronicapava@gmail.com>
2022-04-26 18:08:13 -0500

    Cambios de estilos menores

--- src/components/Navbar/Navegacion.js
+++ src/components/Navbar/Navegacion.js
@@ -1,18 +1,16 @@
-import { Nav, Navbar } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
 
 const Navegacion = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" className="navbar-light bg-light fixed-top">
-      <Navbar.Brand href="/">
+    <nav className="navbar navbar-expand-lg navbar-light bg-white">
+      <div className="container-fluid">
         <img src="https://cdn-icons-png.flaticon.com/512/1857/1857924.png"
           alt="logofoodka" width="30" height="24"
           className="d-inline-block align-text-top" />
-        Foodka
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-      </Navbar.Collapse>
-    </Navbar>
+        FOODKA
+      </div >
+
+    </nav>
   )
 };
 
