7d810d5012b6e23de5060ab544c0f1782cc4d332
lucia2207 <ludef15@gmail.com>
2022-04-26 19:09:31 -0300

    marge exitoso sin conflictos , uwu, ahora si commit pa que quede lindo parte1

--- src/components/Pages/ContactUs.jsx
+++ src/components/Pages/ContactUs.jsx
@@ -2,7 +2,25 @@ import React from "react"
 
 const Contact = () => {
   return <React.Fragment>
-  <h2>Contáctenos</h2>
+    <div id = "seccionContacto" className="container">
+    <h3>Contáctenos</h3>
+    <div className="row">
+  <div className="col">
+    <input type="text" className="form-control" placeholder="Primer Nombre" aria-label="First name"/>
+  </div>
+  <div className="col">
+    <input type="text" className="form-control" placeholder="Apellido" aria-label="Last name"/>
+  </div>
+</div>
+  <div className="mb-3">
+  <label for="exampleFormControlInput1" className="form-label">Direccion de correo</label>
+  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+</div>
+<div className="mb-3">
+  <label for="exampleFormControlTextarea1" className="form-label">Dejanos tu comentario aquí...</label>
+  <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+</div>
 </React.Fragment>
 }
 
