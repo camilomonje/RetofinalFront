114947a70d00c2446335eab1439501a93a389c41
Luis Camilo Monje Perdomo <91104820+camilomonje@users.noreply.github.com>
2022-04-27 16:17:28 -0500

    Revert "Dev lucia"

--- src/components/Navbar/Navegacion.js
+++ src/components/Navbar/Navegacion.js
@@ -1,25 +1,16 @@
-import { Nav, Navbar} from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Navbar } from 'react-bootstrap';
 
 const Navegacion = () => {
-  return(
-      <Navbar collapseOnSelect expand="lg" className="navbar-light bg-light fixed-top">
-        <Navbar.Brand href="/">
-          <img src="https://cdn-icons-png.flaticon.com/512/1857/1857924.png"
-            alt="logofoodka" width="30" height="24"
-            className="d-inline-block align-text-top" />
-          Foodka
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-          <ul className="navbar-nav mb-2 mb-lg-0">
-            <Link to="/nosotros" className="nav-link">Sobre Nosotros</Link>
-            <Link to="/contacto" className="nav-link">Contáctenos</Link>
-          </ul>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-white">
+      <div className="container-fluid">
+        <img src="https://cdn-icons-png.flaticon.com/512/1857/1857924.png"
+          alt="logofoodka" width="30" height="24"
+          className="d-inline-block align-text-top" />
+        FOODKA
+      </div >
+
+    </nav>
   )
 };
 
